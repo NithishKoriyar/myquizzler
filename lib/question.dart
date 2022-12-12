155a9788e82056ac16621f5ad8932c1cde396464
Nithish <nithishk6765@gmail.com>
2022-12-12 14:50:01 +0530

    questions and answers

---
+++ lib/question.dart
@@ -0,0 +1,22 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question ({String q ="", bool a=true}) {
+
+    questionText = q;
+    questionAnswer = a;
+  }
+}
+
+// class Question {
+//   String questionText;
+//   bool questionAnswer;
+
+//   Question ({String q, bool a}) {
+
+//     questionText = q;
+//     questionAnswer = a;
+//   }
+// }
+
